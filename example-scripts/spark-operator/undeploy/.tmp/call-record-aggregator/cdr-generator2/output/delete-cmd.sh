47c98c66b4780407de5945c089a917dc5a7a0e1c
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-04-20 17:51:25 +0100

    cleanup

--- example-scripts/spark-operator/undeploy/.tmp/call-record-aggregator/cdr-generator2/output/delete-cmd.sh
+++
@@ -1,2 +0,0 @@
-    kubectl delete sparkapplications.sparkoperator.k8s.io call-record-aggregator-cdr-generator2 --namespace call-record-aggregator
-    kubectl wait --for=delete pods -l sparkoperator.k8s.io/app-name=call-record-aggregator-cdr-generator2 --namespace call-record-aggregator
