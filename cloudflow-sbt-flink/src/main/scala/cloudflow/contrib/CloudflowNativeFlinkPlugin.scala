a02b770e18914188ad14b945a2d043428aea3ba1
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2021-08-09 19:09:29 +0200

    Flink 1.13.2.

--- cloudflow-sbt-flink/src/main/scala/cloudflow/contrib/CloudflowNativeFlinkPlugin.scala
+++ cloudflow-sbt-flink/src/main/scala/cloudflow/contrib/CloudflowNativeFlinkPlugin.scala
@@ -65,8 +65,8 @@ object CloudflowNativeFlinkPlugin extends AutoPlugin {
     IO.write(flinkEntrypoint, flinkEntrypointContent)
 
     val scalaVersion = (ThisProject / scalaBinaryVersion).value
-    val flinkVersion = "1.13.1"
-    val flinkPackageVersion = "1.13.1"
+    val flinkVersion = "1.13.2"
+    val flinkPackageVersion = "1.13.2"
 
     val flinkTgz = s"flink-${flinkVersion}-bin-scala_${scalaVersion}.tgz"
 
