495698780da2aef0305926c465894a36a7d6e623
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2022-01-21 10:51:44 +0100

    Upgrade flink and some minor dep fixes.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   object Versions {
     val cloudflowVersion = "2.3.0-RC33"
 
-    val flinkVersion = "1.14.2"
+    val flinkVersion = "1.14.3"
     val sparkVersion = "3.2.0"
     val akka = "2.6.14"
     val jackson = "2.11.4"
