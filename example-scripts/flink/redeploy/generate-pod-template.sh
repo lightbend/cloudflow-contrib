62a8f66c103acfa5a37462ee86ad46e0f30934da
francisco <franciscolopezsancho@gmail.com>
2022-02-03 09:22:52 +0000

    quoting volume names on deployment scripts

--- example-scripts/flink/redeploy/generate-pod-template.sh
+++ example-scripts/flink/redeploy/generate-pod-template.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 STREAMLET_FOLDER=$1
 if [ -z "$STREAMLET_FOLDER" ]; then
@@ -23,13 +24,13 @@ jq -rc '.kubernetes.pods.pod.containers.container."volume-mounts" | keys[]' "${S
   while IFS='' read volume_name; do
     # echo "Volume name: $volume_name"
 
-    is_pvc=$(jq -rc ".kubernetes.pods.pod.volumes.${volume_name}.pvc" "${STREAMLET_FOLDER}secrets/pods-config.conf")
+    is_pvc=$(jq -rc ".kubernetes.pods.pod.volumes.\"${volume_name}\".pvc" "${STREAMLET_FOLDER}secrets/pods-config.conf")
     if [ -z $is_pvc ] || [ "$is_pvc" = "null" ] || [ "$is_pvc" = "" ]; then
       # Not a PVC
       true
     else
       pvc_name="$volume_name"
-      pvc_claim_name=$(jq -r ".kubernetes.pods.pod.volumes.${volume_name}.pvc.name" "${STREAMLET_FOLDER}secrets/pods-config.conf")
+      pvc_claim_name=$(jq -r ".kubernetes.pods.pod.volumes.\"${volume_name}\".pvc.name" "${STREAMLET_FOLDER}secrets/pods-config.conf")
 
       # Write the ouput file and exit
       # TODO improve especially error handling
--- example-scripts/flink/setup-example-rbac.sh
+++ example-scripts/flink/setup-example-rbac.sh
@@ -9,6 +9,6 @@ fi
 kubectl create serviceaccount flink-service-account --namespace "${APPLICATION}"
 kubectl --namespace "${APPLICATION}" apply -f cluster-role.yaml
 
-yq e ".subjects[0].namespace = \"${APPLICATION}\"" cluster-role-binding.yaml > cluster-role-binding.tmp.yaml
+yq e ".subjects.[0].namespace = \"${APPLICATION}\"" cluster-role-binding.yaml > cluster-role-binding.tmp.yaml
 kubectl --namespace "${APPLICATION}" apply -f cluster-role-binding.tmp.yaml
 rm cluster-role-binding.tmp.yaml
