2269254120679f8c095fdaef4cdfef17be3b151e
Scala Steward <scala_steward@virtuslab.com>
2023-04-01 17:24:04 +0000

    Update joda-time to 2.12.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
   object TestDeps {
 
     val scalatestJunit = "org.scalatestplus" %% "junit-4-13" % s"${Versions.scalaTest}.0" % Test
-    val jodaTime = "joda-time" % "joda-time" % "2.10.14"
+    val jodaTime = "joda-time" % "joda-time" % "2.12.5"
     val scalaxmlSpark = "org.scala-lang.modules" %% "scala-xml" % "1.2.0"
   }
 
